04550b45d5e8c8aa25fbe0126ff42ea72da0a897
Manoj <H.Manoj4355@student.leedsbeckett.ac.uk>
2023-10-09 12:02:44 +0100

    abcd

---
+++ marks.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    marks=[50,20,30,45,60]
+    highest=max(marks)
+    #print(highest)
+    # lowest=min(marks)
+    #print(lowest)
+    mean=sum(marks)/len(marks)
+    #print(mean)
+
+
