adb5071b0ba282e06ef19a8ae65ff68482b6539a
hannahmanoj <hannah.manoj@gmail.com>
2023-10-16 11:03:48 +0100

    abcd

---
+++ Week2.py
@@ -0,0 +1,116 @@
+#task1
+#total = 100
+#print("The total is", total)
+#task2
+#total = total + 99
+#print("The total is now", total)
+#total = total * 100
+#print("The total is now", total)
+
+#task3
+#total = total - 1
+#print("The total is", total)
+#total1 = total * 4
+#print("The total is", total)
+total2 = total / 2
+#print("The total is", total)
+
+#task3
+#avg = total1+total2 / 2
+#print(avg)
+
+#task4
+#print(type(1000))
+#print(type(100.111))
+#print(type('hello'))
+#print(type('hello'))
+#print(type(100/5))
+#print(type(100//5))
+
+#task5
+#print("ABC" * 10)
+#* operator multiplies the given string
+
+#task6
+#name=input('what is your name?')
+#address=input('what is your address?')
+#contact=int(input('what is your contact no?'))
+#print(name/n, address/n, contact )
+#print(len(name))
+
+#task7
+#age = input("Enter your age ")
+#print("in one year your age will be", age + 1)
+#this code does not work because the input is an integer
+
+#age = input("Enter your age ")
+#age = int(age)
+#print("in one year your will be", age + 1)
+
+#task8
+#n1=int(input("enter 1st number:"))
+#n2=int(input("enter 2nd number:"))
+#product=n1*n2
+#print(product)
+
+#task9
+#comment = 'I would have 'thought' you knew better!'
+#print(comment)
+#it results in a syntax error
+
+#task10
+#print("This text includes characters such as '\\', '\"' and \"\'\", \n this is a new line that stars with a tab, \n    this new line starts with two tabs.")
+
+#task11
+#print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n hello there! \n \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+
+#task12
+#print("this text spans tree lines,")
+#print("and includes both single (')")
+#print('and double quotes (")')
+
+#task13
+#surname = "Palin"
+#initial = surname[2]
+#print(initial)
+
+#task14
+#initial = surname[10]
+#print(initial)
+#show error bcz the no is out of range
+
+#task15
+#initial = surname[-2]
+#print(initial)
+
+#task16
+#surname = "Palin"
+#middle = surname[1:]
+#print(middle)
+
+#middle=surname[:-1]
+#print(middle)
+
+#task17
+#primes = [2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+#firstfour = primes[0:4]
+#print(firstfour)
+
+#task18
+#names = ["Tim", "Bill", "Graeme"]
+#names.insert(2,'bill')
+#names.insert(3, 'sarah')
+#print(names)
+
+#task19
+#nums = [1,2,3] * 5
+#print(nums)
+
+#task20
+#The assignment operator, denoted by the “=” symbol, is the operator that is used to assign values to variables in Python.
+#data types include boolean, string, list, integer.
+#An argument is a value that is passed to a function when it is called
+#indexing is accessing certain elements in a list using their index value
+#Slicing is cutting up a list to add new elements
+#Mutable means that changes can be made
+#Immutable means that changes cannot be made
---
+++ Week4.py
@@ -0,0 +1,44 @@
+#task 1
+#def multiply(x,y=1):
+    #if y!=1:
+       # print(x*y)
+    #else:
+       # print(x*x)
+
+#multiply(4)
+#multiply(2,3)
+
+#task 2
+#def someFunc(x, y, z):
+    #print("x is", x, "\ny is", y, "\nz is", z)
+
+
+#someFunc(y=9,x=8,z=0)
+#someFunc(z=0,y=9,x=8)
+
+
+#task 3
+#def calcAve(numbers):
+    #total = 0
+    #for num in numbers:
+      #  total += num
+    #print(total/len(numbers)
+
+#calcAve(8)
+
+#task4
+#import math
+#hypot = lambda a,b : print(math.sqrt(a * a + b * b))
+#print(type(hypot))
+
+#hypot(3,4)
+
+#task
+
+
+
+
+
+
+
+
