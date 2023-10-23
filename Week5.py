9fc788d0ddf1e981d103cab725ef2f191dcee42b
Manoj <H.Manoj4355@student.leedsbeckett.ac.uk>
2023-10-23 10:48:19 +0100

    abcd

---
+++ Week5.py
@@ -0,0 +1,20 @@
+python first_prog.py
+# inputs a digit from user to be checked as odd or even
+number = input("Enter a number: ")
+# int function converts the input to an integer
+number = int(number)
+# prints the number
+print("The numbered entered was", number)
+# to check if the no is odd or even we use modulus, if remainder is 0 its even and if its anything else it's odd
+if (number % 2) == 0 and (number % 10) == 0:
+    print("That is an even number")
+    print("The number is divisible by 10")
+
+elif (number % 2) == 0 and (number % 10) != 0:
+    print("That is an even number")
+    print("The number is not divisible by 10")
+
+
+else:
+    print("That is an odd number")
+    print("The number is not divisible by 10")
