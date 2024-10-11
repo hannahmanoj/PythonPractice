fe24ef1bef45e03c7378112950982898026e29df
Hannah <145972702+hannahmanoj@users.noreply.github.com>
2024-10-11 21:59:34 +0100

    Portfolio1.py

--- Portfolio1.py
+++ Portfolio1.py
@@ -1,11 +1,11 @@
-#print("hello world!")
+print("hello world!")
 
 
-#name=input("enter your name: ")
-#print("hello", name)
+name=input("enter your name: ")
+print("hello", name)
 
-#print("38.8C temp in Fahrenheit, ", (38.4* 9 / 5) + 32,'F')
+print("38.8C temp in Fahrenheit, ", (38.4* 9 / 5) + 32,'F')
 
 
-#print("batting average is: ", 48426/(1014-162))
+print("batting average is: ", 48426/(1014-162))
 
