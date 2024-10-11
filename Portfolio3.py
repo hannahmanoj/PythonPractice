f275e12e3cd1105cbab9ca20f591b24b48bfc241
Hannah <145972702+hannahmanoj@users.noreply.github.com>
2024-10-11 22:02:53 +0100

    Portfolio3.py

--- Portfolio3.py
+++ Portfolio3.py
@@ -1,83 +1,83 @@
 #question1
-#name=input("enter your name:")
-#if name:
-    #print("hello name")
-#else:
-   # print("hello stranger")
+name=input("enter your name:")
+if name = "":
+    print("hello stranger")
+else:
+    print("hello", name)
 
 #question2
-#Password=input("choose password")
-#newpass=input("enter your new password")
-#if Password==newpass:
-    #print("password set")
-#else:
-   # print("ERROR")
+Password=input("choose password")
+newpass=input("enter your new password")
+if Password==newpass:
+    print("password set")
+else:
+    print("ERROR")
 
 #question3
-#Password=input("choose password")
-#if len(Password)>8 and len(Password)<=12:
-    #newpass=input("enter your new password")
-#else:
- #   print("password should be between 8 and 12 characters")
-#if Password==newpass:
- #   print("password set")
-#else:
- #   print("ERROR")
+Password=input("choose password")
+if len(Password)>8 and len(Password)<=12:
+    newpass=input("enter your new password")
+else:
+    print("password should be between 8 and 12 characters")
+if Password==newpass:
+    print("password set")
+else:
+    print("ERROR")
 
 #question4
-#Password=input("choose password")
-#BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
-#for x in BAD_PASSWORDS:
- #   if Password==x:
-  #      print("too easily guessed! try again")
-   #     break
+Password=input("choose password")
+BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
+for x in BAD_PASSWORDS:
+    if Password==x:
+        print("too easily guessed! try again")
+        break
 
-#if len(Password)>8 and len(Password)<=12:
- #   newpass=input("enter your new password")
-#else:
- #   print("password should be between 8 and 12 characters")
+if len(Password)>8 and len(Password)<=12:
+    newpass=input("enter your new password")
+else:
+    print("password should be between 8 and 12 characters")
 
-#if Password==newpass:
- #   print("password set")
-#else:
- #   print("ERROR")
+if Password==newpass:
+    print("password set")
+else:
+    print("ERROR")
 
 #question5
-#while True:
-    #Password=input("choose password")
-    #BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
-    #for x in BAD_PASSWORDS:
-       # if Password==x:
-          #  print("too easily guessed! try again")
-          #  break
+while True:
+    Password=input("choose password")
+    BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
+    for x in BAD_PASSWORDS:
+        if Password==x:
+            print("too easily guessed! try again")
+            break
 
-    #if len(Password)>8 and len(Password)<=12:
-      #  newpass=input("enter your new password")
-      #  break
-    #else:
-      #  print("password should be between 8 and 12 characters")
+    if len(Password)>8 and len(Password)<=12:
+        newpass=input("enter your new password")
+        break
+    else:
+        print("password should be between 8 and 12 characters")
 
-#if Password==newpass:
-   # print("password set")
+if Password==newpass:
+    print("password set")
 
 
-#else:
-   # print("ERROR")
+else:
+    print("ERROR")
 
 #guestion6
-#for i in range(13):
- #   print(i, 'X 7 =', i*7)
+for i in range(13):
+   print(i, 'X 7 =', i*7)
 
 
 #question7
-#table=int(input("enter the number of the table you would like to display: "))
-#for i in range(13):
-   # print(i, 'X', table, '=', i * table)
+table=int(input("enter the number of the table you would like to display: "))
+for i in range(13):
+    print(i, 'X', table, '=', i * table)
 
 #question8
-#table=int(input("enter the number of tables you would like to display: "))
-#for i in range(12,-1,-1):
-    #print(i, 'X', table, '=', i * table)
+table=int(input("enter the number of tables you would like to display: "))
+for i in range(12,-1,-1):
+    print(i, 'X', table, '=', i * table)
 
 
 
